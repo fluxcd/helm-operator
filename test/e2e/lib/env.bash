b323bb328beec4be2f8807c2b909ea63db81535a
Hidde Beydals <hello@hidde.co>
2019-11-20 11:00:09 +0100

    e2e: scaffold testing framework

---
+++ test/e2e/lib/env.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export E2E_NAMESPACE=flux-e2e
+export DEMO_NAMESPACE=demo
+ROOT_DIR=$(git rev-parse --show-toplevel)
+export ROOT_DIR
+export E2E_DIR="${ROOT_DIR}/test/e2e"
+export FIXTURES_DIR="${E2E_DIR}/fixtures"
+KNOWN_HOSTS=$(cat "${FIXTURES_DIR}/known_hosts")
+export KNOWN_HOSTS
+GITCONFIG=$(cat "${FIXTURES_DIR}/gitconfig")
+export GITCONFIG
