691aeccbaa374c81e1975717c84f08ddf8b65d14
Hidde Beydals <hello@hidde.co>
2019-12-11 17:17:55 +0100

    helm/v2: use download manager for dep updates
    This avoids having to shell out to the `helm2` binary while still
    maintaining the same functionality. It aligns with the approach
    used in `helm/v3`.

--- pkg/helm/v2/dependency.go
+++ pkg/helm/v2/dependency.go
@@ -1,68 +1,37 @@
 package v2
 
 import (
-	"errors"
 	"fmt"
-	"os"
-	"os/exec"
-	"path/filepath"
+
+	"github.com/go-kit/kit/log"
+
+	"k8s.io/helm/pkg/downloader"
 )
 
 func (h *HelmV2) DependencyUpdate(chartPath string) error {
-	var hasLockFile bool
+	repositoryConfigLock.RLock()
+	defer repositoryConfigLock.RUnlock()
 
-	// sanity check: does the chart directory exist
-	if chartPath == "" {
-		return errors.New("empty path to chart supplied")
+	out := &logWriter{h.logger}
+	man := downloader.Manager{
+		Out: out,
+		ChartPath: chartPath,
+		HelmHome: helmHome(),
+		Getters: getters,
 	}
-	chartInfo, err := os.Stat(chartPath)
-	switch {
-	case os.IsNotExist(err):
-		return fmt.Errorf("chart path %s does not exist", chartPath)
-	case err != nil:
-		return err
-	case !chartInfo.IsDir():
-		return fmt.Errorf("chart path %s is not a directory", chartPath)
+	return man.Update()
+}
+
+// logWriter wraps a `log.Logger` so it can be used as an `io.Writer`
+type logWriter struct {
+	log.Logger
+}
+
+func (l *logWriter) Write(p []byte) (n int, err error) {
+	origLen := len(p)
+	if len(p) > 0 && p[len(p)-1] == '\n' {
+		p = p[:len(p)-1] // Cut terminating newline
 	}
-
-	// check if the requirements file exists
-	reqFilePath := filepath.Join(chartPath, "requirements.yaml")
-	reqInfo, err := os.Stat(reqFilePath)
-	if err != nil || reqInfo.IsDir() {
-		return nil
-	}
-
-	// We are going to use `helm dep build`, which tries to update the
-	// dependencies in charts/ by looking at the file
-	// `requirements.lock` in the chart directory. If the lockfile
-	// does not match what is specified in requirements.yaml, it will
-	// error out.
-	//
-	// If that file doesn't exist, `helm dep build` will fall back on
-	// `helm dep update`, which populates the charts/ directory _and_
-	// creates the lockfile. So that it will have the same behaviour
-	// the next time it attempts a release, remove the lockfile if it
-	// was created by helm.
-	lockfilePath := filepath.Join(chartPath, "requirements.lock")
-	info, err := os.Stat(lockfilePath)
-	hasLockFile = (err == nil && !info.IsDir())
-	if !hasLockFile {
-		defer os.Remove(lockfilePath)
-	}
-
-	cmd := exec.Command("helm2", "repo", "update")
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		return fmt.Errorf("could not update repo: %s", string(out))
-	}
-
-	cmd = exec.Command("helm2", "dep", "build", ".")
-	cmd.Dir = chartPath
-
-	out, err = cmd.CombinedOutput()
-	if err != nil {
-		return fmt.Errorf("could not update dependencies in %s: %s", chartPath, string(out))
-	}
-
-	return nil
+	l.Log("info", fmt.Sprintf("%s", p))
+	return origLen, nil
 }
