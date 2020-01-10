adcf9d3abf0749794c53e1a174a3b8799ceda0d8
Hidde Beydals <hello@hidde.co>
2020-01-10 14:21:45 +0100

    helm: replace 'status' with 'get'

--- pkg/helm/v2/get.go
+++ pkg/helm/v2/get.go
@@ -3,21 +3,21 @@ package v2
 import (
 	"strings"
 
+	"github.com/pkg/errors"
+
 	helmv2 "k8s.io/helm/pkg/helm"
 
 	"github.com/fluxcd/helm-operator/pkg/helm"
 )
 
-func (h *HelmV2) Status(releaseName string, opts helm.StatusOptions) (*helm.Release, error) {
-	// We use `ReleaseContent` here as `ReleaseStatus` does not return
-	// the full release, which is required to construct a `helm.Release`.
+func (h *HelmV2) Get(releaseName string, opts helm.GetOptions) (*helm.Release, error) {
 	res, err := h.client.ReleaseContent(releaseName, helmv2.ContentReleaseVersion(int32(opts.Version)))
 	if err != nil {
 		err = statusMessageErr(err)
 		if strings.Contains(err.Error(), "not found") {
 			return nil, nil
 		}
-		return nil, err
+		return nil, errors.Wrapf(err, "failed to retrieve release [%s]", releaseName)
 	}
 	return releaseToGenericRelease(res.Release), nil
 }
