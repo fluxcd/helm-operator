ff896daf7999882519f7e84c4259f2b42943a821
Hidde Beydals <hello@hidde.co>
2020-02-05 15:55:05 +0100

    build: generate Helm and `deploy/` CRDs

--- hack/tools/tools.go
+++ hack/tools/tools.go
@@ -7,5 +7,9 @@ package tools
 // +build tools
 
 import (
-	_ "k8s.io/code-generator"
+	_ "k8s.io/code-generator/cmd/client-gen"
+	_ "k8s.io/code-generator/cmd/deepcopy-gen"
+	_ "k8s.io/code-generator/cmd/informer-gen"
+	_ "k8s.io/code-generator/cmd/lister-gen"
+	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 )
