8c7a8fbd866ddb23e134375b79cb28aa5f769dd7
Mohammed Naser <mnaser@vexxhost.com>
2020-09-16 19:21:11 -0400

    Update utils.go

--- pkg/helm/utils.go
+++ pkg/helm/utils.go
@@ -1,9 +1,20 @@
 package helm
 
 import (
+	"reflect"
+
 	"github.com/google/go-cmp/cmp"
 )
 
 func Diff(j *Release, k *Release) string {
-	return cmp.Diff(j.Values, k.Values) + cmp.Diff(j.Chart, k.Chart)
+	opt := cmp.FilterValues(func(x, y interface{}) bool {
+		isNumeric := func(v interface{}) bool {
+			return v != nil && reflect.TypeOf(v).ConvertibleTo(reflect.TypeOf(float64(0)))
+		}
+		return isNumeric(x) && isNumeric(y)
+	}, cmp.Transformer("T", func(v interface{}) float64 {
+		return reflect.ValueOf(v).Convert(reflect.TypeOf(float64(0))).Float()
+	}))
+
+	return cmp.Diff(j.Values, k.Values, opt) + cmp.Diff(j.Chart, k.Chart, opt)
 }
